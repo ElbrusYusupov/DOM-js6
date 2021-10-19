956712ac8aa1647c21b876f8741bd44e09e4c548
ElbrusYusupov <yusupoffup@gmail.com>
2021-10-19 21:54:18 +0300

    вапвап

--- JS/dom-six.js
+++ JS/dom-six.js
@@ -35,20 +35,14 @@
 //     keypressCounter += 1;
 // }
 
-const listWithId = document.querySelector('#menu');
-listWithId.style.textTransform = 'uppercase';
-listWithId.style.fontSize = '24px';
-// console.log(listWithId);
+const message = document.querySelector("#message");
+console.log(message.value); // Default textarea message
 
-const listWithClass = document.querySelector('.menu');
-// console.log(listWithClass);
+//===================================================
+const activeLink = document.querySelector(".btn.active");
+console.log(activeLink.href); // https://s.codepen.io/about
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
-
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-// console.log(menuItemsByClass);
-
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = 'tomato';
-// console.log(firstMenuItem);
+//===================================================
+const image = document.querySelector(".image");
+console.log(image.src); // https://placeimg.com/640/480/animals
+image.src = 'https://placeimg.com/640/480/tech';
