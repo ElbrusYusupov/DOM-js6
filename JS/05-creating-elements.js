9d40061dd6fcbdef1fe5b995fdaef9563b3a14b9
ElbrusYusupov <yusupoffup@gmail.com>
2021-10-23 21:29:51 +0300

    ;lklkl;

--- JS/05-creating-elements.js
+++ JS/05-creating-elements.js
@@ -9,7 +9,7 @@
 const titleEl = document.createElement('h1');
 titleEl.classList.add('page-title');
 titleEl.textContent = 'Это заголовок страницы :)';
-// console.log(titleEl);
+console.log(titleEl);
 
 // document.body.appendChild(titleEl);
 
